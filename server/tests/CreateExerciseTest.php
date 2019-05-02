b524d0c09112639b9e61dfd28be6cfaaa9f20cb3
Jerome <j.butel@wakeonweb.com>
2019-05-02 14:08:30 +0200

    Add first exercise test

---
+++ server/tests/CreateExerciseTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class CreateExerciseTest extends KernelTestCase
+{
+    /** @var HttpClientInterface */
+    private $client;
+
+    /** @var int */
+    private $exerciseId;
+
+    protected function setUp(): void
+    {
+        $this->client = HttpClient::create([
+            'base_uri' => 'http://127.0.0.1:8000',
+            'headers' => [
+                'Accept' => 'application/ld+json',
+            ],
+        ]);
+    }
+
+    public function testShouldCreateExerciseWithMCQ()
+    {
+        $response = $this->client->request('POST', '/api/exercises', [
+            'json' => ['name' => 'test - exercise with MCQ'],
+        ]);
+
+        $this->exerciseId = $response->toArray()['id'];
+
+        $this->assertSame(201, $response->getStatusCode());
+    }
+
+    protected function tearDown(): void
+    {
+        $this->client->request('DELETE', '/api/exercises/'.$this->exerciseId);
+    }
+}
