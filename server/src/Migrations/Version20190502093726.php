090f0e60e98307ea43ae8e263ae18345dcf446f6
Jerome <j.butel@wakeonweb.com>
2019-05-02 11:40:23 +0200

    Add migration and fixtures for exercise

---
+++ server/src/Migrations/Version20190502093726.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20190502093726 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    /**
+     * @param Schema $schema
+     *
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function up(Schema $schema): void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SEQUENCE exercise_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE exercise (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+    }
+
+    /**
+     * @param Schema $schema
+     *
+     * @throws \Doctrine\DBAL\DBALException
+     */
+    public function down(Schema $schema): void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE exercise_id_seq CASCADE');
+        $this->addSql('DROP TABLE exercise');
+    }
+}
